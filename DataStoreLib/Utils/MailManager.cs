a7f8697f088a72ec02e33d7d4530fd6b413a2aa1
Viren <viren@itmonarch.com>
2014-03-20 20:29:07 +0530

    Add MailManager
    Add MailManager class for sending mails.

---
+++ DataStoreLib/Utils/MailManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStoreLib.Utils
+{
+    /// <summary>
+    /// This class use to send mails
+    /// </summary>
+    public class MailManager
+    {
+        /// <summary>
+        /// Send an email
+        /// </summary>
+        /// <param name="toAddress">mail message to-Address</param>
+        /// <param name="fromAddress">mail message from-Address</param>
+        /// <param name="subject">mail message subject</param>
+        /// <param name="body">mail message body</param>
+        public bool SendMail(string toAddress, string fromAddress, string subject, string body)
+        {
+            return true;
+        }
+    }
+}
