c1c89dede45ec77c8918e8e8d4cf49ce5d18d7df
vasim <vasim.ahmed@itmonarch.com>
2014-03-20 21:53:16 +0530

    made mail Part
    made mail Part from configuration or by user

---
+++ DataStoreLib/Storage/AffiliationTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataStoreLib.Utils;
+using DataStoreLib.Models;
+using DataStoreLib.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace DataStoreLib.Storage
+{
+    public class AffilationTable : Table
+    {
+         //protected CloudTable _table;
+        public AffilationTable(CloudTable table) : base(table)
+        {
+        }
+
+        internal static Table CreateTable(CloudTable table)
+        {
+            return new AffilationTable(table);
+        }
+
+        protected override string GetParitionKey()
+        {
+            return AffilationEntity.PARTITION_KEY;
+        }
+
+    }
+}
--- DataStoreLib/Utils/MailManager.cs
+++ DataStoreLib/Utils/MailManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+
+using System.Net.Mail;
 
 namespace DataStoreLib.Utils
 {
@@ -20,6 +23,51 @@ namespace DataStoreLib.Utils
         /// <param name="body">mail message body</param>
         public bool SendMail(string toAddress, string fromAddress, string subject, string body)
         {
+            try
+            {
+
+
+                string sUser = ConfigurationManager.AppSettings["User"];
+                string sPassword = ConfigurationManager.AppSettings["Password"];
+                string sHost = ConfigurationManager.AppSettings["Host"];
+                int iPort = Convert.ToInt32(ConfigurationManager.AppSettings["Port"]);
+                bool bEnableSsl = Convert.ToBoolean(ConfigurationManager.AppSettings["EnableSsl"]);
+                // bool bUseDefaultCredentials = Convert.ToBoolean(ConfigurationManager.AppSettings["UseDefaultCredentials"]);
+
+                MailMessage mailMessage = new MailMessage();
+                mailMessage.IsBodyHtml = true;
+
+                if (fromAddress == "")
+                {
+                    mailMessage.From = new MailAddress(ConfigurationManager.AppSettings["fromAddress"]);  // Key from the config file
+                }
+                else
+                {
+                    mailMessage.From = new MailAddress(fromAddress);
+                }
+
+                mailMessage.To.Add(toAddress); // Key from the config file
+
+                if (subject == "")
+                {
+                    mailMessage.Subject = ConfigurationManager.AppSettings["subject"]; // Key from the config file
+                }
+                else
+                {
+                    mailMessage.Subject = subject;
+                }
+
+                mailMessage.Body = body;
+                SmtpClient smtpServer = new SmtpClient(sHost);  // Key the mail server
+                smtpServer.Port = iPort;
+                smtpServer.Credentials = new System.Net.NetworkCredential(ConfigurationManager.AppSettings["fromAddress"], ConfigurationManager.AppSettings["Password"]);
+                smtpServer.EnableSsl = bEnableSsl;
+                smtpServer.Send(mailMessage);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
             return true;
         }
     }
