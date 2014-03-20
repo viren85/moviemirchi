5cd1525dd1ec06bff11f7a5be2cc5a6df8f45dcb
Viren <viren@itmonarch.com>
2014-03-20 21:31:29 +0530

    modified api
    modified api

--- MvcWebRole1/Controllers/api/SendMailController.cs
+++ MvcWebRole1/Controllers/api/SendMailController.cs
@@ -7,9 +7,12 @@ using System.Web.Http;
 
 namespace MvcWebRole1.Controllers.api
 {
+    /// <summary>
+    /// get all user who has liked param celeb and send an email to all of them
+    /// </summary>
     public class SendMailController : BaseController
     {
-        //get api/SendMail?type=[value]
+        //get api/SendMail?name=[actor, producer, writer etc.]
         protected override string ProcessRequest()
         {
             return "";
