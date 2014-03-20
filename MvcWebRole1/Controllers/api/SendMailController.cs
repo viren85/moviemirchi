b49e02d437cfdde0a45118320a1c18b6fbed6180
Viren <viren@itmonarch.com>
2014-03-20 21:15:33 +0530

    Add API for sending mail
    Add api which is send mail by javascript.

---
+++ MvcWebRole1/Controllers/api/SendMailController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MvcWebRole1.Controllers.api
+{
+    public class SendMailController : BaseController
+    {
+        //get api/SendMail?type=[value]
+        protected override string ProcessRequest()
+        {
+            return "";
+        }
+    }
+}
