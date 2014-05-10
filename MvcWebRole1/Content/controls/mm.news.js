a5d0bbbe28c4c31ece81f289cbdf75ace0233547
bhangaleashish <bhangaleashish@gmail.com>
2014-05-10 15:43:05 -0700

    Making news code use the rotating pattern - to be styled

--- MvcWebRole1/Content/controls/mm.news.js
+++ MvcWebRole1/Content/controls/mm.news.js
@@ -4,19 +4,85 @@
 }
 
 var ShowNews = function (data) {
-    var jdata = JSON.parse(data);
 
+    var control = null;
     var news = [];
-    for (var v in jdata) {
-        var t = jdata[v];
-        news.push({
-            title: (t.Title || ""),
-            description: (t.Description || ""),
+
+    var renderControl = function (news) {
+        control = new NewsControl(".news-container", news);
+        control.startTimer(12000);
+    };
+
+    (function (feedUrls) {
+        var accumulate = function (data) {
+
+            if (data && data.responseData && data.responseData.feed && data.responseData.feed.entries) {
+                data.responseData.feed.entries.forEach(function (f) {
+                    news.push(f);
+                });
+            }
+
+            if (startingpoint.deferred.state() === "resolved") {
+                complete();
+            }
+        };
+        var sort = function () {
+            news.sort(function (a, b) {
+                return new Date(b.publishedDate) - new Date(a.publishedDate);
+            });
+        };
+        var complete = function () {
+            if (news && news.length > 0) {
+                sort();
+                renderControl(news);
+                ////render(news);
+            }/* else {
+            displayError("We are unable to show news at this time");
+        }*/
+        };
+
+        function DeferredAjax(opts) {
+            this.deferred = $.Deferred();
+            this.feedUrl = opts.feedUrl;
+        }
+
+        DeferredAjax.prototype.invoke = function () {
+            var self = this;
+            return $.ajax({
+                type: "GET",
+                url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(self.feedUrl),
+                dataType: "JSON",
+                success: function (data) {
+                    self.deferred.resolve();
+                    accumulate(data);
+                }
+            });
+        };
+
+        DeferredAjax.prototype.promise = function () {
+            return this.deferred.promise();
+        };
+
+        var startingpoint = $.Deferred();
+        startingpoint.resolve();
+
+        $.each(feedUrls, function (ix, feedUrl) {
+            var da = new DeferredAjax({
+                feedUrl: feedUrl
+            });
+            $.when(startingpoint)
+                .then(function () {
+                    da.invoke();
+                });
+            startingpoint = da;
         });
-    }
-    
-    var ne = new NewsControl(".news-container", news);
-    ne.startTimer(12000);
+
+    })([
+            "http://www.bollywoodnewsworld.com/category/bollywood-news/feed",
+            "http://www.glamsham.com/rss/glamrss_scoops.xml",
+            "http://www.bollywoodhungama.com/rss/news.xml",
+            "http://feeds.hindustantimes.com/HT-Bollywood"
+    ]);
 }
 
 var newsData;
@@ -37,7 +103,7 @@ var iterator = function (a, n) {
 };
 
 var NewsControl = function (selector, data) {
-
+    debugger;
     var _selector = selector;
     var _data = data;
     var _n = 4;
@@ -48,61 +114,23 @@ var NewsControl = function (selector, data) {
     (function () {
         $(selector).html(
             "<div class='tweet-container'>" +
-                "<div class='tweet-left'></div>" +
-                "<div class='tweet-right'></div>" +
+                "<div class='tweet-left'>" +
+                    "<div class=\"tweet-cell\"></div>" +
+                    "<div class=\"tweet-cell\"></div>" +
+                "</div>" +
+                "<div class='tweet-right'>" +
+                    "<div class=\"tweet-cell\"></div>" +
+                    "<div class=\"tweet-cell\"></div>" +
+                "</div>" +
             "</div>");
-
-        var left = $(_selector + " .tweet-container .tweet-left");
-        var right = $(_selector + " .tweet-container .tweet-right");
-
-        var html =
-            "<div class=\"tweet-cell\">" +
-                "<div class=\"tweet-user\"></div>" +
-                "<div class=\"tweet-content\"></div>" +
-            "</div>";
-        left.append(html).append(html);
-        right.append(html).append(html);
 
         _cells = $(_selector + " .tweet-container").find(".tweet-cell");
     })();
 
     NewsControl.prototype.render = function () {
         var newsItems = _iterator();
-        $.each(newsItems, function (index, n) {
-            var cell = $(_cells[index]);
-            var children = cell.children();
-            children.fadeOut(1000, function () {
-                var txt = n.description.length > 100 ? n.description.substring(0, 100) + "..." : n.description;
-                cell.find(".tweet-user").text(n.title);
-                cell.find(".tweet-content").text(txt);
-                children.fadeIn(2000);
-            });
-        });
-    };
 
-    NewsControl.prototype.startTimer = function (timeout) {
-        timeout = (timeout) ? timeout : 6000;
-        var threadRender = this.render;
-        _timer = setInterval(function () {
-            threadRender();
-        }, timeout);
-    };
-
-    NewsControl.prototype.stopTimer = function () {
-        if (_timer) {
-            clearInterval(_timer);
-            _timer = null;
-        }
-    };
-
-    // First render
-    NewsControl.prototype.render();
-};
-/*((function ShowNewsControl(feedUrls, selector) {
-
-    var render = function (entries) {
-
-        entries.forEach(function (entry) {
+        $.each(newsItems, function (index, entry) {
 
             var getValue = function (k) {
                 var el = entry[k];
@@ -152,7 +180,7 @@ var NewsControl = function (selector, data) {
                 }
 
                 var html =
-                "<li class='news-item'>" +
+                //"<li class='news-item'>" +
                     "<div class='news-title'>" + newsTitleSpan + "</div>" +
                     "<div class='news-content-container'>" +
                         "<div class='" + publishDateClass + "'>" +
@@ -165,97 +193,35 @@ var NewsControl = function (selector, data) {
                         "<div class='news-author news-link'><a target=\"_new\" href=\"" + link + "\">" +
                             (isOK(author) ? author : "Link") +
                         "</a></div>" +
-                    "</div>" +
-                "</li>";
+                    "</div>";// +
+                //"</li>";
 
-                $(selector).append(html);
-            }
-        });
-
-        // Adding the pager
-        // TODO: here .parent() is hardcoded - that should be removed
-        PreparePaginationControl($(selector).parent(), {
-            "pagerContainer": "news-pager",
-            "tilesInPage": 3,
-            "totalTileCount": $(selector).find("li.news-item").length,
-            "pagerContainerId": "news-pager",
-        });
-
-        entries = null;
-    };
-
-    var displayError = function (v) {
-        var html =
-            "<div class='entryContainer'><span class='error'>" + v + "</span></div>";
-        $(selector).append(html);
-    };
-
-    var entries = [];
-    var accumulate = function (v) {
-        v.forEach(function (f) {
-            entries.push(f);
-        });
-
-        if (startingpoint.deferred.state() === "resolved") {
-            complete();
-        }
-    };
-    var sort = function () {
-        entries.sort(function (a, b) {
-            return new Date(b.publishedDate) - new Date(a.publishedDate);
-        });
-    };
-    var complete = function () {
-        if (entries && entries.length > 0) {
-            sort();
-            render(entries);
-        } else {
-            displayError("We are unable to show news at this time");
-        }
-    };
-
-    function DeferredAjax(opts) {
-        this.deferred = $.Deferred();
-        this.feedUrl = opts.feedUrl;
-    }
-
-    DeferredAjax.prototype.invoke = function () {
-        var self = this;
-        return $.ajax({
-            type: "GET",
-            url: 'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(self.feedUrl),
-            dataType: "JSON",
-            success: function (data) {
-                self.deferred.resolve();
-                if (data && data.responseData && data.responseData.feed && data.responseData.feed.entries) {
-                    accumulate(data.responseData.feed.entries);
-                }
+                var cell = $(_cells[index]);
+                //var children = cell.children();
+                cell.fadeOut(1000, function () {
+                    cell.empty();
+                    cell.append(html);
+                    cell.fadeIn(2000);
+                });
             }
         });
     };
 
-    DeferredAjax.prototype.promise = function () {
-        return this.deferred.promise();
+    NewsControl.prototype.startTimer = function (timeout) {
+        timeout = (timeout) ? timeout : 6000;
+        var threadRender = this.render;
+        _timer = setInterval(function () {
+            threadRender();
+        }, timeout);
     };
 
-    var startingpoint = $.Deferred();
-    startingpoint.resolve();
+    NewsControl.prototype.stopTimer = function () {
+        if (_timer) {
+            clearInterval(_timer);
+            _timer = null;
+        }
+    };
 
-    $.each(feedUrls, function (ix, feedUrl) {
-        var da = new DeferredAjax({
-            feedUrl: feedUrl
-        });
-        $.when(startingpoint)
-            .then(function () {
-                da.invoke();
-            });
-        startingpoint = da;
-    });
-
-})([
-            "http://www.bollywoodnewsworld.com/category/bollywood-news/feed",
-            "http://www.glamsham.com/rss/glamrss_scoops.xml",
-            "http://www.bollywoodhungama.com/rss/news.xml",
-            "http://feeds.hindustantimes.com/HT-Bollywood"
-], ".news-container ul"));
-*/
+    // First render
+    NewsControl.prototype.render();
+};
--- MvcWebRole1/Content/controls/mm.twitter.js
+++ MvcWebRole1/Content/controls/mm.twitter.js
@@ -19,8 +19,6 @@ var ShowTweets = function (data) {
     twtr.startTimer(12000);
 }
 
-var tweetData;
-
 var iterator = function (a, n) {
     var current = 0,
         l = a.length;
@@ -98,35 +96,3 @@ var TwitterControl = function (selector, data) {
     // First render
     TwitterControl.prototype.render();
 };
-
-//$(document).ready(function () {
-
-//    tweetData = {
-//        tweets:
-//            [
-//                { "twitterid": "@Bernadette", "text": "Occaecat consequat pariatur incididunt deserunt esse magna. Veniam incididunt culpa nisi esse duis ipsum." },
-//                { "twitterid": "@Duran", "text": "Lorem et Lorem proident elit pariatur consectetur ullamco deserunt. Labore est est aliqua mollit quis adipisicing consectetur." },
-//                { "twitterid": "@Sloan", "text": "Lorem sunt in adipisicing reprehenderit enim enim occaecat minim fugiat culpa. Pariatur id adipisicing sit nisi dolor ex aliqua officia aliquip irure ipsum." },
-//                { "twitterid": "@Woods", "text": "Duis anim non cillum excepteur enim sint exercitation nisi. Commodo aute nostrud id irure." },
-//                { "twitterid": "@Bonnie", "text": "Veniam aliquip minim sunt Lorem. Cupidatat minim amet aliquip ad in ipsum dolor ad consequat aute." },
-//                { "twitterid": "@Benita", "text": "Duis qui nisi ut velit eiusmod ut velit elit laboris mollit amet reprehenderit dolore. Amet et laborum nostrud do mollit ullamco aute exercitation quis consectetur laboris labore anim ea." },
-//                { "twitterid": "@Clemons", "text": "Excepteur et anim sunt anim deserunt tempor velit voluptate dolor magna id non. Cupidatat tempor dolore minim ex." },
-//                { "twitterid": "@Maldonado", "text": "Duis et consectetur ea do occaecat et id amet. Qui tempor non reprehenderit sint nostrud ad laboris." },
-//                { "twitterid": "@Horton", "text": "Reprehenderit tempor amet commodo do dolore laboris aute. Proident ad ad excepteur id occaecat." },
-//                { "twitterid": "@Margie", "text": "Eu ullamco duis magna non. Ad culpa pariatur cillum tempor minim Lorem." },
-//                { "twitterid": "@Villarreal", "text": "Fugiat aliqua proident ex cillum. Velit consectetur consectetur aute dolor esse ex laborum." },
-//                { "twitterid": "@Carrillo", "text": "Consequat adipisicing eiusmod labore aliqua sunt tempor commodo deserunt sint dolore et anim exercitation. Consequat adipisicing cillum est fugiat." },
-//                { "twitterid": "@Earlene", "text": "Laboris in exercitation anim aliquip aliqua reprehenderit laboris ut occaecat in proident. Consectetur non dolore amet tempor commodo magna nostrud adipisicing incididunt esse incididunt amet eu." },
-//                { "twitterid": "@Bertha", "text": "Commodo veniam cillum ullamco sint voluptate. Excepteur officia cillum labore excepteur sit." },
-//                { "twitterid": "@Delacruz", "text": "Irure pariatur consequat ea laborum aliqua culpa amet sit occaecat ad Lorem cillum dolor. Proident do eiusmod ipsum proident id dolore anim laborum labore non adipisicing Lorem." },
-//                { "twitterid": "@Sharlene", "text": "Fugiat adipisicing et quis laborum esse eiusmod magna incididunt minim. Veniam ullamco commodo tempor et cupidatat minim tempor." },
-//                { "twitterid": "@Jeanne", "text": "Officia consectetur laboris eiusmod sint sint non culpa irure. Nisi quis laborum qui nulla ea consectetur laboris consequat ea ut fugiat voluptate non." },
-//                { "twitterid": "@Nannie", "text": "Voluptate laborum mollit nisi ad incididunt et ipsum fugiat voluptate enim fugiat. Ex consequat ex eiusmod quis sunt dolore Lorem Lorem." },
-//                { "twitterid": "@Charlene", "text": "Commodo nulla officia sint et excepteur voluptate nostrud aute commodo laboris non. Nisi eu officia culpa magna anim minim tempor consectetur." },
-//                { "twitterid": "@Effie", "text": "Consectetur est velit ipsum aute et irure voluptate. Incididunt officia ullamco duis eu consequat ad." }
-//            ]
-//    };
-
-//    var twtr = new TwitterControl(".tweets", tweetData.tweets);
-//    twtr.startTimer(12000);
-//});
