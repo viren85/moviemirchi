132053e4574415bf9a5a567457d5d94050293314
vasim <vasim.ahmed@itmonarch.com>
2014-05-20 20:59:50 +0530

    Clean up - removed references of unwanted files

--- MvcWebRole1/App_Start/BundleConfig.cs
+++ MvcWebRole1/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace MvcWebRole1
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
@@ -25,7 +25,7 @@ namespace MvcWebRole1
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
 
-            /*bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
                         "~/Content/themes/base/jquery.ui.selectable.css",
