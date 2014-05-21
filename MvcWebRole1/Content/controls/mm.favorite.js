ae3f0edacaeedd2e3be5eef3e8b3a36843cc694b
vasim <vasim.ahmed@itmonarch.com>
2014-05-21 17:54:06 +0530

    Clean up - moved login, favorite related code in separate files

---
+++ MvcWebRole1/Content/controls/mm.favorite.js
@@ -0,0 +1,289 @@
+﻿function PopulatingUserFavorite() {
+    var url = "api/GetCastByType?t=all&l=3";
+    CallHandler(url, OnSuccessPopulatingUserFavorite);
+}
+
+function OnSuccessPopulatingUserFavorite(result) {
+    result = JSON.parse(result);
+
+    if (result.Status == "Ok") {
+        /* getting actor */
+        for (var a = 0; a < result.Actor.length; a++) {
+            //alert(result.Actor[a].name);
+            var span = $("<span/>");
+            var checkBox = $("<input/>");
+            $(checkBox).attr("type", "checkbox");
+            $(checkBox).attr("id", "fav_actor_" + a);
+
+            var checkBoxLabel = $("<label/>");
+            $(checkBoxLabel).attr("for", "fav_actor_" + a);
+            $(checkBoxLabel).html(result.Actor[a].name);
+
+            $(span).append(checkBox);
+            $(span).append(checkBoxLabel);
+
+            $("#fav_actor").append(span);
+        }
+        // adding textbox for search acotr
+        var span = $("<span/>");
+        $(span).attr("class", "fav-span");
+        var searchActor = $("<input/>");
+        $(searchActor).attr("type", "text");
+        $(searchActor).attr("id", "txtSearchActor");
+        $(searchActor).attr("placeholder", "Actor Name...");
+        $(searchActor).attr("class", "form-control");
+
+        $(searchActor).keyup(function (e) {
+            //Fetching the textbox value.
+            if (e.keyCode == 40 || e.keyCode == 38) {
+            }
+            else {
+                var query = $(this).val();
+                //Calling GetItems method.
+                autoCompleteTextBox1(query, "AutoComplete/AutoCompleteActors", "autoCompleteActor", "actorUL", "txtSearchActor", "hfMovieId");
+            }
+        });
+
+        var autoCompleteActor = $("<div>");
+        $(autoCompleteActor).attr("id", "autoCompleteActor");
+
+        $(autoCompleteActor).append(searchActor);
+
+        $(span).append(autoCompleteActor);
+        //$(span).append(searchActor);
+        $("#fav_actor").append(span);
+
+        /* getting director */
+        for (var d = 0; d < result.Director.length; d++) {
+
+            var span = $("<span/>");
+            var checkBox = $("<input/>");
+            $(checkBox).attr("type", "checkbox");
+            $(checkBox).attr("id", "fav_director_" + d);
+
+            var checkBoxLabel = $("<label/>");
+            $(checkBoxLabel).attr("for", "fav_director_" + d);
+            $(checkBoxLabel).html(result.Director[d].name);
+
+            $(span).append(checkBox);
+            $(span).append(checkBoxLabel);
+
+            $("#fav_director").append(span);
+        }
+        // adding textbox for search acotr
+        var dSpan = $("<span/>");
+        $(dSpan).attr("class", "fav-span");
+        var searchDirector = $("<input/>");
+        $(searchDirector).attr("type", "text");
+        $(searchDirector).attr("id", "txtSearchDirector");
+        $(searchDirector).attr("placeholder", "Director Name...");
+        $(searchDirector).attr("class", "form-control");
+
+        $(searchDirector).keyup(function (e) {
+            //Fetching the textbox value.
+            if (e.keyCode == 40 || e.keyCode == 38) {
+            }
+            else {
+                var query = $(this).val();
+                //Calling GetItems method.
+                autoCompleteTextBox1(query, "AutoComplete/AutoCompleteDirectors", "autoCompleteDirector", "directorUL", "txtSearchDirector", "hfMovieId");
+            }
+        });
+
+        var autoCompleteDirector = $("<div>");
+        $(autoCompleteDirector).attr("id", "autoCompleteDirector");
+
+        $(autoCompleteDirector).append(searchDirector);
+
+        $(dSpan).append(autoCompleteDirector);
+
+        //$(dSpan).append(searchDirector);
+        $("#fav_director").append(dSpan);
+
+        /* getting music director*/
+        for (var md = 0; md < result.MusicDirector.length; md++) {
+
+            var span = $("<span/>");
+            var checkBox = $("<input/>");
+            $(checkBox).attr("type", "checkbox");
+            $(checkBox).attr("id", "fav_musicdirector_" + md);
+
+            var checkBoxLabel = $("<label/>");
+            $(checkBoxLabel).attr("for", "fav_musicdirector_" + md);
+            $(checkBoxLabel).html(result.MusicDirector[md].name);
+
+            $(span).append(checkBox);
+            $(span).append(checkBoxLabel);
+
+            $("#fav_musicdirector").append(span);
+        }
+
+        // adding textbox for search acotr
+        var mdSpan = $("<span/>");
+        $(mdSpan).attr("class", "fav-span");
+        var searchMusicDirector = $("<input/>");
+        $(searchMusicDirector).attr("type", "text");
+        $(searchMusicDirector).attr("id", "txtSearchMusicDirector");
+        $(searchMusicDirector).attr("placeholder", "Music Director Name...");
+        $(searchMusicDirector).attr("class", "form-control");
+
+        $(searchMusicDirector).keyup(function (e) {
+            //Fetching the textbox value.
+            if (e.keyCode == 40 || e.keyCode == 38) {
+            }
+            else {
+                var query = $(this).val();
+                //Calling GetItems method.
+                autoCompleteTextBox1(query, "AutoComplete/AutoCompleteMusicDirectors", "autoCompleteMusicDirector", "musicDirectorUL", "txtSearchMusicDirector", "hfMovieId");
+            }
+        });
+
+        var autoCompleteMusicDirector = $("<div>");
+        $(autoCompleteMusicDirector).attr("id", "autoCompleteMusicDirector");
+
+        $(autoCompleteMusicDirector).append(searchMusicDirector);
+
+        $(mdSpan).append(autoCompleteMusicDirector);
+
+        //$(mdSpan).append(searchMusicDirector);
+        $("#fav_musicdirector").append(mdSpan);
+    }
+}
+
+
+function SaveUserFavorite() {
+    var actorName = $("#txtSearchActor").val();
+    var directorName = $("#txtSearchDirector").val();
+    var musicDirectorName = $("#txtSearchMusicDirector").val();
+    var userId = $("#hfUserId").val();
+
+    var isValid = false;
+
+    var FavoriteList = [];
+
+    $("#fav_actor").find('input[type="checkbox"]').each(function () {
+        if ($(this).attr("checked")) {
+            isValid = true;
+            var acName = $(this).next("label").html();
+            FavoriteList.push({ "Type": "Actor", "Name": acName });
+        }
+    });
+
+    $("#fav_director").find('input[type="checkbox"]').each(function () {
+        if ($(this).attr("checked")) {
+            isValid = true;
+            var dirName = $(this).next("label").html();
+            //alert(dirName);
+            FavoriteList.push({ "Type": "Director", "Name": dirName });
+        }
+    });
+
+    $("#fav_musicdirector").find('input[type="checkbox"]').each(function () {
+        if ($(this).attr("checked")) {
+            isValid = true;
+            var musDirName = $(this).next("label").html();
+            //alert(musDirName);
+            FavoriteList.push({ "Type": "Music Director", "Name": musDirName });
+        }
+    });
+
+    $("#fav_genre").find('input[type="checkbox"]').each(function () {
+        if ($(this).attr("checked")) {
+            isValid = true;
+            var genre = $(this).next("label").html();
+            //alert(genre);
+            FavoriteList.push({ "Type": "Genre", "Name": genre });
+        }
+    });
+
+    if (actorName != "") {
+        isValid = true;
+        FavoriteList.push({ "Type": "Actor", "Name": actorName });
+    }
+
+    if (directorName != "") {
+        isValid = true;
+        FavoriteList.push({ "Type": "Director", "Name": directorName });
+    }
+
+    if (musicDirectorName != "") {
+        isValid = true;
+        FavoriteList.push({ "Type": "Music Director", "Name": musicDirectorName });
+    }
+
+    if (userId != "") {
+        //setCookie("favoriteId", "userid", 365);
+    }
+
+    var cFavoriteId = getCookie("favoriteId");
+
+    if (isValid) {
+        CallHandler("api/SaveUserFavorite?u=" + userId + "&c=" + cFavoriteId + "&d=" + encodeURI(JSON.stringify(FavoriteList)), OnSuccessSaveUserFavorite);
+    }
+    else {
+        $("#favStatus").attr("style", "display:block");
+        $("#favStatus").html("Please enter actor name or select genre");
+    }
+}
+
+function OnSuccessSaveUserFavorite(result) {
+    result = JSON.parse(result);
+
+    if (result.Status == "Ok") {
+
+        if (result.Message == "Set Cookie") {
+            setCookie("favoriteId", result.FavoriteId, 365);
+        }
+
+        ClearUserFavoriteControls();
+
+        $("#favStatus").removeAttr("class");
+        $("#favStatus").attr("class", "alert alert-success");
+        $("#favStatus").attr("style", "display:block");
+        $("#favStatus").html("Successfully saved your favorite list.");
+
+        var intverval = setInterval(function () {
+            $(".user-fav").slideToggle("slow");
+            clearInterval(intverval);
+            $("#favStatus").html("");
+            $("#favStatus").hide();
+        }, 10000);
+
+    }
+    else {
+        $("#favStatus").attr("style", "display:block");
+        $("#favStatus").html("Ops! there is some error. Please try again.");
+
+        if (result.Message == "No Updated") {
+            $("#favStatus").html("You already set your favorites. Login for update.");
+        }
+
+        var intverval = setInterval(function () {
+            $("#favStatus").hide(500);
+            $("#favStatus").html("");
+            clearInterval(intverval);
+        }, 10000);
+    }
+}
+
+function ClearUserFavoriteControls() {
+    $("#fav_actor").find('input[type="checkbox"]').each(function () {
+        $(this).attr("checked", false);
+    });
+
+    $("#fav_director").find('input[type="checkbox"]').each(function () {
+        $(this).attr("checked", false);
+    });
+
+    $("#fav_musicdirector").find('input[type="checkbox"]').each(function () {
+        $(this).attr("checked", false);
+    });
+
+    $("#fav_genre").find('input[type="checkbox"]').each(function () {
+        $(this).attr("checked", false);
+    });
+
+    $("#txtSearchActor").val("");
+    $("#txtSearchDirector").val("");
+    $("#txtSearchMusicDirector").val("");
+}
---
+++ MvcWebRole1/Content/controls/mm.login.js
@@ -0,0 +1,197 @@
+﻿
+/*------ Login ----------------------------------------*/
+function authenticateUser() {
+    var isValid = true;
+    try {
+        var username = $("#signin_email").val();
+        var loginPassword = $("#signin_password").val();
+
+        if (username == "") {
+            $("#loginError").html("Email address require.");
+            $("#loginError").show();
+            $("#signin_email").focus();
+            isValid = false;
+            return;
+        }
+
+        if (loginPassword == "") {
+            $("#loginError").html("Password require.");
+            $("#loginError").show();
+            $("#signin_password").focus();
+            isValid = false;
+            return;
+        }
+        //CallHandler("Login/UserLogin", encodeURI(JSON.stringify(hfLogin)));
+
+        if (isValid) {
+            var hflogin = ({ "UserName": username, "Email": username, "Password": loginPassword });
+            // $("#hfLogin").val(JSON.stringify(hfLogin));
+
+            $.ajax({
+                url: BASE_URL + 'Login/UserLogin',
+                data: { "hfLogin": JSON.stringify(hflogin) },
+                type: 'Post',
+                dataType: 'json',
+                success: ShowSuccessMessageLogin,
+                error: function (xhr, status, error) {
+                }
+            });
+        }
+
+
+    } catch (ex) {
+        $("#loginError").html("There some error please try again.");
+        $("#loginError").show();
+    }
+
+
+}
+
+function ShowSuccessMessageLogin(result) {
+    if (result.Status == "Ok") {
+        window.location = BASE_URL + 'Home/Index';
+    } else if (result.Status == "Require") {
+        $("#loginError").html("Username and Password require.");
+        $("#loginError").show();
+        ClearLoginformData();
+    } else if (result.Status == "Error") {
+        $("#loginError").html("Login Failed, Try again.");
+        $("#loginError").show();
+    }
+}
+
+
+function ClearLoginformData() {
+
+    $("#signin_email").val("");
+    $("#signin_password").val("");
+    $("#loginError").hide("");
+    //$("#Login"). data-dismiss("modal");
+}
+
+
+/*Register the user from popup  -------------------*/
+function RegisterUser() {
+    var isValid = true;
+    try {
+        var fname = $("#FirstName").val();
+        var lname = $("#LastName").val();
+        var email = $("#Email1").val();
+        var pwd = $("#password2").val();
+        var confirmPassword = $("#password3").val();
+
+        if (fname == "") {
+            $("#registerError").html("Please provide First Name.");
+            $("#registerError").show();
+            $("#FirstName").focus();
+            isValid = false;
+            return;
+        }
+
+
+        if (lname == "") {
+            $("#registerError").html("Please provide Last Name.");
+            $("#registerError").show();
+            $("#LastName").focus();
+            isValid = false;
+            return;
+        }
+
+        if (email == "") {
+            $("#registerError").html("Please provide email address.");
+            $("#registerError").show();
+            $("#Email1").focus();
+            isValid = false;
+            return;
+        }
+
+        if (pwd == "") {
+            $("#registerError").html("Please provide password.");
+            $("#registerError").show();
+            $("#password2").focus();
+            isValid = false;
+            return;
+        }
+
+        if (confirmPassword == "") {
+            $("#registerError").html("Please provide confirm password.");
+            $("#registerError").show();
+            $("#password3").focus();
+            isValid = false;
+            return;
+        }
+
+        if (!IsEmailValid(email)) {
+            $("#registerError").html("Please provide valid email address.");
+            $("#registerError").show();
+            $("#Email1").focus();
+            isValid = false;
+            return;
+        }
+
+        if (pwd != confirmPassword) {
+            $("#registerError").html("Password and confirm password does not match.");
+            $("#registerError").show();
+            $("#password3").focus();
+            isValid = false;
+            return;
+        }
+
+        if (isValid) {
+            var user = {
+                "FirstName": fname,
+                "LastName": lname,
+                "UserName": email,
+                "Email": email,
+                "Password": pwd,
+                "Mobile": confirmPassword // mobile no use as confirm password
+            };
+            //$("#hfAffilations").val(JSON.stringify(user));
+            $.ajax({
+
+                url: 'Login/Register',
+                data: { "userJson": JSON.stringify(user) },
+                type: 'POST',
+                dataType: 'json',
+                success: ShowSuccessMessage,
+                error: function (xhr, status, error) {
+                }
+            });
+        }
+    } catch (e) {
+        $("#registerError").html("There some error please try again.");
+        $("#registerError").show();
+    }
+}
+
+function ShowSuccessMessage(result) {
+
+    if (result.Status == "Ok") {
+        $("#successStatusR").html("You are successfully register. Please login to access your account.");
+        $("#successStatusR").show();
+        ClearformData();
+    } else if (result.Status == "Error") {
+        if (result.Message != undefined) {
+            $("#registerError").html(result.Message);
+        }
+
+        $("#registerError").show();
+    }
+    else {
+        $("#registerError").html("Unable to register. Please try again after some time.");
+        $("#registerError").show();
+    }
+
+
+}
+
+function ClearformData() {
+    $("#FirstName").val("");
+    $("#LastName").val("");
+    $("#Email1").val("");
+    $("#password2").val("");
+    $("#password3").val("");
+    $("#registerError").hide("");
+}
+
+/* end Login*/
