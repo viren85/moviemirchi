01a81d00621a6eed3aaf0e67a8317f09f2ace466
bhangaleashish <bhangaleashish@gmail.com>
2014-05-23 02:01:22 -0700

    Updating NEWS control to not go blank while animating. [now similar to Twtr control]

--- MvcWebRole1/Content/controls/mm.news.js
+++ MvcWebRole1/Content/controls/mm.news.js
@@ -106,23 +106,30 @@ var NewsControl = function (selector, data) {
                 var newsTitleText = new Util().GetEllipsisText(entry.Title, 95);
 
                 var html =
-                    "<div class='news-title'>" + newsTitleText + "</div>" +
-                    "<div class='news-content-container'>" +
-                        "<div class='news-content-text'>" +
-                            (isImageUrl ? "<div class='left'><img class=\"img\" src=\"" + entry.Image + "\" alt=\"Image\" /></div>" : "") +
-                            "<div class='" + (isImageUrl ? "news-right-content" : "both") + "'>" + new Util().GetEllipsisText(entry.Description, 180) + "</div>" +
+                    "<div>" +
+                        "<div class='news-title'>" + newsTitleText + "</div>" +
+                        "<div class='news-content-container'>" +
+                            "<div class='news-content-text'>" +
+                                (isImageUrl ? "<div class='left'><img class=\"img\" src=\"" + entry.Image + "\" alt=\"Image\" /></div>" : "") +
+                                "<div class='" + (isImageUrl ? "news-right-content" : "both") + "'>" + new Util().GetEllipsisText(entry.Description, 180) + "</div>" +
+                            "</div>" +
                         "</div>" +
-                    "</div>" +
-                    "<div class='news-author news-link'><a target=\"_new\" href=\"" + entry.Link + "\">" +
-                            (entry.Source ? entry.Source : "Link") +
-                        "</a></div>";
+                        "<div class='news-author news-link'>" +
+                            "<a target=\"_new\" href=\"" + entry.Link + "\">" +
+                                (entry.Source ? entry.Source : "Link") +
+                            "</a>" +
+                        "</div>" +
+                    "</div>";
 
                 //// TODO: this is workaround for news dupe issue - set the index back to index
                 var cell = $(_cells[Math.round(index / 2) - 1]);
-                cell.fadeOut(1000, function () {
-                    cell.empty();
-                    cell.append(html);
-                    cell.fadeIn(2000);
+                var cc = cell.find("div")[0];
+                var ael = cc ? $(cc) : cell;
+
+                ael.fadeOut(1000, function () {
+                    ael.empty();
+                    ael.append(html);
+                    ael.fadeIn(2000);
                 });
             }
         });
