1b42cfca1baca9b637c0577755a6a3a4ecfb7842
bhangaleashish <bhangaleashish@gmail.com>
2014-07-01 22:31:25 -0700

    Working WebRole on Ashish's environment

--- MvcWebRole1/Content/controls/mm.tube.js
+++ MvcWebRole1/Content/controls/mm.tube.js
@@ -1,5 +1,5 @@
 ﻿//var PUBLIC_BLOB_URL = "http://127.0.0.1:10000/devstoreaccount1/posters/";
-var PUBLIC_BLOB_URL = "https://moviemirchistorage1.blob.core.windows.net/posters/";
+var PUBLIC_BLOB_URL = "https://moviemirchistorage.blob.core.windows.net/posters/";
 
 var pagerControlCounter = 1;
 function GetTubeControl(sectionTitle, tileContainer, pagerContainerId, classId) {
