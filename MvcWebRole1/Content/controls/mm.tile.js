29f728b0d46a17f04f770de8ac692e9417d15c5a
Vasim <vasim.ahmed@itmonarch.com>
2014-07-08 21:22:00 +0530

    Integrating new tiles UI

--- MvcWebRole1/Content/controls/mm.tile.js
+++ MvcWebRole1/Content/controls/mm.tile.js
@@ -3,7 +3,6 @@
 
     var poster = [];
     poster = JSON.parse(movie.Posters);
-    //var src = (poster != null && poster.length > 0) ? "/Posters/Images/" + poster[poster.length - 1] : "/Posters/Images/default-movie.jpg"; 
     var src = (poster != null && poster.length > 0) ? PUBLIC_BLOB_URL + poster[poster.length - 1] : PUBLIC_BLOB_URL + "default-movie.jpg";
 
     var anchor = $("<a/>");
@@ -18,10 +17,10 @@
 
     var synopsis = movie.Synopsis.length > 500 ? movie.Synopsis.substring(0, 500) + "..." : movie.Synopsis;
 
-    var html =
+    var html1 =
     "<div id=\"picAndCaption\" class=\"viewingDiv " + movie.UniqueName + "\">" +
         "<div id=\"imageContainer\" class=\"viewer\" style=\"height: 400px;\">" +
-            "<img id=\"imageEl\" onerror=\"LoadDefaultImage(this);\" onload=\"MovieImageLoaded(this);\" class=\"movie-poster shownImage\" title=\"" + movie.Name + "\" alt=\"" + movie.Name + "\" src=\"" + src + "\" style=\"margin: auto;\">" +
+            "<img id=\"imageEl\" onerror=\"LoadDefaultImage(this);\" onload=\"MovieImageLoaded(this);\" class=\"movie-poster shownImage\" title=\"" + movie.Name + "\" alt=\"" + movie.Name + "\" src=\"" + src + "\" style=\"margin: auto;\"/>" +
             "<div class=\"captionAndNavigate\">" +
                 "<div id=\"captionCredit\" class=\"multimediaCaption\">" +
                     "<div id=\"photoCaption\">" +
@@ -34,6 +33,37 @@
                 "</div>" +
             "</div>" +
         "</div>" +
+    "</div>";
+
+    var html =
+    "<div id=\"picAndCaption\" class=\"viewingDiv " + movie.UniqueName + "\">" +
+        "<div id=\"imageContainer\" class=\"viewer\" style=\"height: 300px;\">" +
+            "<img id=\"imageEl\" onerror=\"LoadDefaultImage(this);\" onload=\"MovieImageLoaded(this);\" class=\"movie-poster shownImage\" title=\"" + movie.Name + "\" alt=\"" + movie.Name + "\" src=\"" + src + "\" style=\"margin: auto;\"/>" +
+        "</div>" +
+        "<div id=\"hover\" style=\"width: 84%; padding: 4%; background-color: white; float: left; height: 175px;border: 1px solid #ddd; box-shadow: -3px 3px 5px #ccc;\">" +
+                    "<div class=\"img-movie-name\">" + movie.Name + "</div>" +
+                    "<div class=\"img-movie-genre\">" + movie.Genre + "</div>" +
+                    "<div class=\"img-movie-date\">" + movie.Month + "</div>" +
+                    GetRateControl(movie.Ratings) +
+        "</div>"
+    /*"<div class=\"captionAndNavigate\" style=\"width:170px;padding: 15px;padding-top: 5px;padding-bottom:5px;\">" +
+            "<div id=\"captionCredit\" style=\"width: 398px;\" class=\"multimediaCaption\">" +
+                "<div id=\"photoCaption\">" +
+                    "<div class=\"img-movie-name\">" + movie.Name + "</div>" +
+                    "<div class=\"img-movie-genre\">" + movie.Genre + "</div>" +
+                    "<div class=\"img-movie-date\">" + movie.Month + "</div>" +
+                    GetRateControl(movie.Ratings) +
+                    "<div class=\"movie-songs\">" +
+                    "<ul>" +
+                    "<li><span>Tu hi Junoon</span><span class='play'></span></li>" +
+                    "<li><span>Malang</span><span class='play'></span></li>" +
+                    "<li><span>Kamli</span><span class='play'></span></li>" +
+                    "</ul>"
+                    + "</div>" +
+                    "<div class=\"movie-synopsis\" style=\"display: none;\">" + synopsis + "</div>" +
+                "</div>" +
+            "</div>" +
+        "</div>" +*/
     "</div>";
 
     anchor.append(html);
@@ -53,7 +83,7 @@ function MovieImageLoaded(img) {
 
     // When image is of small size, it leaves lot of white spaces next to tile. When image is of large size (Dhoom), it overlaps the next image
     // Hence keeping the height + width of fix size.
-    var newWidth = 263;
+    var newWidth = 200;
 
     /*
     if (newWidth > 263) {
@@ -63,7 +93,7 @@ function MovieImageLoaded(img) {
         newWidth = 263;
     }*/
 
-    $(img).css("width", newWidth + "px").css("height", "400px");
+    $(img).css("width", newWidth + "px").css("height", "300px");
 }
 
 function LoadDefaultImage(element) {
--- MvcWebRole1/Content/movie.core.js
+++ MvcWebRole1/Content/movie.core.js
@@ -57,67 +57,70 @@ function onSuccessLoadCurrentMovies(result) {
 
     if (result.length > 0) {
         MOVIES = result;
-
+        console.log(1);
         // adding images        
         for (var i = 0; i < result.length; i++) {
             var list = PopulatingMovies(result[i], "movie-list");
         }
-
+        console.log(2);
         /*The image width/height shall be calculated once the image is fully loaded*/
         var width = $(document).width();
 
         $(".movie-list").find("img").each(function () {
             var ratio = this.width / this.height;
-            var newWidth = 400 * ratio;
-            $(this).width(newWidth + "px").height("380px");
+            var newWidth = 300 * ratio;
+            $(this).width(newWidth + "px").height("300px");
 
-            if (newWidth > 250)
-                $(this).width("250px");
+            if (newWidth > 200)
+                $(this).width("200px");
         });
-
-        ScaleElement($(".movie-list ul"));
+        console.log(3);
+        ScaleElement1($(".movie-list ul"));
 
         // movie-list
         PreparePaginationControl($(".movie-list"));
+        console.log(4);
         //PreparePaginationControl($(".news-container"));
 
         $(window).resize(function () {
             PreparePaginationControl($(".movie-list"));
         });
+        console.log(5);
     }
 }
 
 function onSuccessLoadUpcomingMovies(result) {
     result = JSON.parse(result);
-
+    console.log(6);
     if (result.length > 0) {
         //MOVIES = result;
-
+        
         // adding images        
         for (var i = 0; i < result.length; i++) {
             var list = PopulatingMovies(result[i], "upcoming-movie-list");
         }
-
+        console.log(7);
         /*The image width/height shall be calculated once the image is fully loaded*/
         var width = $(document).width();
 
         $(".upcoming-movie-list").find("img").each(function () {
             var ratio = this.width / this.height;
-            var newWidth = 400 * ratio;
-            $(this).width(newWidth + "px").height("380px");
+            var newWidth = 300 * ratio;
+            $(this).width(newWidth + "px").height("300px");
 
-            if (newWidth > 250)
-                $(this).width("250px");
+            if (newWidth > 200)
+                $(this).width("200px");
         });
-
-        ScaleElement($(".upcoming-movie-list ul"));
+        console.log(8);
+        ScaleElement1($(".upcoming-movie-list ul"));
 
         // movie-list
         PreparePaginationControl($(".upcoming-movie-list"), { pagerContainerId: "upcoming-pager" });
-
+        console.log(9);
         $(window).resize(function () {
             PreparePaginationControl($(".upcoming-movie-list"), { pagerContainerId: "upcoming-pager" });
         });
+        console.log(10);
     }
 }
 
@@ -144,4 +147,33 @@ function GetLinks(html, type) {
 
     links = links.substring(0, links.lastIndexOf("|"));
     return links;
+}
+
+function ScaleElement1(element) {
+    var currentElement = null;
+    $(element).find("li.movie").each(function () {
+        $(this).find("#picAndCaption,.captionAndNavigate").hover(function () {
+            var element = this;
+            $(element).find(".captionAndNavigate").each(function () {
+                $(this).css("position", "absolute").css("top", "60px").css("height", "230px");
+                /*$(this).find(".img-movie-name").each(function () { $(this).show(); });
+                $(this).find(".img-movie-genre").each(function () { $(this).show(); });
+                $(this).find(".img-movie-date").each(function () { $(this).show(); });
+                $(this).find(".movie-songs ul").each(function () { $(this).show(); });*/
+            });
+
+
+        }, function () {
+            var element = this;
+            $(element).find(".captionAndNavigate").each(function () {
+                //setTimeout(function(){},200);
+                /*$(this).find(".img-movie-name").each(function () { $(this).hide(); });
+                $(this).find(".img-movie-genre").each(function () { $(this).hide(); });
+                $(this).find(".img-movie-date").each(function () { $(this).hide(); });
+                $(this).find(".movie-songs ul").each(function () { $(this).hide(); });*/
+                $(this).css("position", "relative").css("top", "auto").css("height", "auto");
+            });
+
+        });
+    });
 }
