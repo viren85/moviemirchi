22ebd66cc44a4d1d539d5c3c4447d4bcd04c9247
bhangaleashish <bhangaleashish@gmail.com>
2014-07-23 09:11:26 -0700

    Execute SmartMonkey once the WebAPIRole starts

---
+++ MvcWebRole1/WebRole.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using System;
+using System.Diagnostics;
+
+namespace MvcWebRole1
+{
+    public class WebRole : RoleEntryPoint
+    {
+        public override bool OnStart()
+        {
+            // For information on handling configuration changes
+            // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
+
+            SetupLuceneIndexes();
+
+            var result = base.OnStart();
+
+            RunSmartMonkey();
+
+            return result;
+        }
+
+        private void SetupLuceneIndexes()
+        {
+            //// TODO: Implement this method
+            ;
+        }
+
+        private static void RunSmartMonkey()
+        {
+            try
+            {
+                // Run SmartMonkey
+                using (Process p = Process.Start("SmartMonkey.exe"))
+                {
+
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
