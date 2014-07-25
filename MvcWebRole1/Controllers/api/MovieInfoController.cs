5eba0af6c432c1c9749e54374e493ab2ea9829fc
bhangaleashish <bhangaleashish@gmail.com>
2014-07-25 08:25:21 -0700

    Cachine movie info per movie

--- MvcWebRole1/Controllers/api/MovieInfoController.cs
+++ MvcWebRole1/Controllers/api/MovieInfoController.cs
@@ -4,6 +4,7 @@ namespace MvcWebRole1.Controllers.api
     using DataStoreLib.Constants;
     using DataStoreLib.Models;
     using DataStoreLib.Storage;
+    using DataStoreLib.Utils;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -18,7 +19,7 @@ namespace MvcWebRole1.Controllers.api
     public class MovieInfoController : BaseController
     {
         private static Lazy<JavaScriptSerializer> jsonSerializer = new Lazy<JavaScriptSerializer>(() => new JavaScriptSerializer());
-        
+
         // get : api/MovieInfo?q={movieId}
         protected override string ProcessRequest()
         {
@@ -32,59 +33,68 @@ namespace MvcWebRole1.Controllers.api
 
             string name = qpParams["q"].ToString().ToLower();
 
-            try
+            string json;
+            if (!CacheManager.TryGet<string>(CacheConstants.MovieInfoJson + name, out json))
             {
-                var tableMgr = new TableManager();
 
-                // get single movie object form database by its unique name
-                var movie = tableMgr.GetMovieByUniqueName(name);
-
-                if (movie != null)
+                try
                 {
-                    MovieInfo movieInfo = new MovieInfo();
-                    movieInfo.movieId = movie.MovieId;
-                    movieInfo.Movie = movie;
+                    var tableMgr = new TableManager();
 
-                    // get reviews for movie by movie id
-                    var reviewList = tableMgr.GetReviewsByMovieId(movie.MovieId);
+                    // get single movie object form database by its unique name
+                    var movie = tableMgr.GetMovieByUniqueName(name);
 
-                    // if reviews not null then add review to review list.
-                    var userReviews = (reviewList != null)? 
-                        reviewList.Select(review =>
-                        {
-                            ReviewerEntity reviewer = tableMgr.GetReviewerById(review.Value.ReviewerId);
-                            ReviewEntity objReview = review.Value as ReviewEntity;
+                    if (movie != null)
+                    {
+                        MovieInfo movieInfo = new MovieInfo();
+                        movieInfo.movieId = movie.MovieId;
+                        movieInfo.Movie = movie;
 
-                            objReview.ReviewerName = reviewer.ReviewerName;
-                            //objReview.OutLink = reviewer.ReviewerImage;
-                            return objReview;
-                        }) :
-                        Enumerable.Empty<ReviewEntity>();
+                        // get reviews for movie by movie id
+                        var reviewList = tableMgr.GetReviewsByMovieId(movie.MovieId);
 
-                    //add reviewlist to movieinfo reviews
-                    movieInfo.MovieReviews = userReviews.ToList();
-                    
-                    // serialize movie object and return.
-                    return jsonSerializer.Value.Serialize(movieInfo);
+                        // if reviews not null then add review to review list.
+                        var userReviews = (reviewList != null) ?
+                            reviewList.Select(review =>
+                            {
+                                ReviewerEntity reviewer = tableMgr.GetReviewerById(review.Value.ReviewerId);
+                                ReviewEntity objReview = review.Value as ReviewEntity;
+
+                                objReview.ReviewerName = reviewer.ReviewerName;
+                                //objReview.OutLink = reviewer.ReviewerImage;
+                                return objReview;
+                            }) :
+                            Enumerable.Empty<ReviewEntity>();
+
+                        //add reviewlist to movieinfo reviews
+                        movieInfo.MovieReviews = userReviews.ToList();
+
+                        // serialize movie object and return.
+                        json = jsonSerializer.Value.Serialize(movieInfo);
+                    }
+                    else
+                    {
+                        // if movie not found then return empty string
+                        json = string.Empty;
+                    }
+
+                    CacheManager.Add<string>(CacheConstants.MovieInfoJson + name, json);
                 }
-                else
+                catch (Exception ex)
                 {
-                    // if movie not found then return empty string
-                    return string.Empty;
+                    // if any error occured then return User friendly message with system error message
+                    // use jsonError here because more custumizable
+                    return jsonSerializer.Value.Serialize(
+                    new
+                    {
+                        Status = "Error",
+                        UserMessage = "Unable to find " + name + " movie.",
+                        ActualError = ex.Message
+                    });
                 }
             }
-            catch (Exception ex)
-            {
-                // if any error occured then return User friendly message with system error message
-                // use jsonError here because more custumizable
-                return jsonSerializer.Value.Serialize(
-                new
-                {
-                    Status = "Error",
-                    UserMessage = "Unable to find " + name + " movie.",
-                    ActualError = ex.Message
-                });
-            }
+
+            return json;
         }
     }
 }
