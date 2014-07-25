4a1b32562d14292fc467b3bbc0792111255e4ef0
bhangaleashish <bhangaleashish@gmail.com>
2014-07-25 08:23:48 -0700

    Now we have a cache manager

---
+++ DataStoreLib/Utils/CacheConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataStoreLib.Utils
+{
+    public static class CacheConstants
+    {
+        #region Entity
+        public const string AllMovieEntities = "AllMovies";
+        public const string UpcomingMovieEntities = "UpcomingMovies";
+        public const string NowPlayingMovieEntities = "NowPlayingMovies";
+        public const string AllMovieEntitiesSortedByName = "AllMoviesSortedByName";
+        #endregion
+
+        #region JSON
+        public const string PopularTagsJson = "PopularTags";
+        #endregion
+
+        #region Json for cetains keys
+        public const string MovieInfoJson = "MovieInfoJson_";
+        #endregion
+    }
+}
---
+++ DataStoreLib/Utils/CacheManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Caching;
+
+namespace DataStoreLib.Utils
+{
+    public static class CacheManager
+    {
+        public static void Add<T>(string key, T o)
+        {
+            HttpRuntime.Cache.Insert(
+                key,
+                o,
+                null,
+                DateTime.Now.AddMinutes(60),
+                Cache.NoSlidingExpiration);
+        }
+
+        public static void Remove(string key)
+        {
+            HttpRuntime.Cache.Remove(key);
+        }
+
+        public static void Clear()
+        {
+            List<string> toRemove = new List<string>();
+            foreach (DictionaryEntry cacheItem in HttpRuntime.Cache)
+            {
+                toRemove.Add(cacheItem.Key.ToString());
+            }
+
+            foreach (string key in toRemove)
+            {
+                HttpRuntime.Cache.Remove(key);
+            }
+        }
+
+        public static bool Exists(string key)
+        {
+            return HttpRuntime.Cache[key] != null;
+        }
+
+        public static bool TryGet<T>(string key, out T value)
+        {
+            value = (T)HttpRuntime.Cache[key];
+            if (value != null)
+            {
+                return true;
+            }
+            else
+            {
+                value = default(T);
+                return false;
+            }
+        }
+    }
+}
