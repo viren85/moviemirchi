839f20d6757d1db8b6658f2a203d035786851389
vasim <vasim.ahmed@itmonarch.com>
2014-07-26 22:13:31 +0530

    Fix - Mobile UX - UI issue

--- MvcWebRole1/Content/controls/mm.search.js
+++ MvcWebRole1/Content/controls/mm.search.js
@@ -38,6 +38,9 @@
 
             // keyCode is 13 for 'Enter' keypress. On Enter we want to treat it as click on Search button
         else if (query.length > 3 || !e.originalEvent || e.keyCode === 13) {
+            if ($(window).width() < 768)
+                $(".nav-bar-container").hide();
+
             getItems(query);
             $("#search-results").show();
         }
