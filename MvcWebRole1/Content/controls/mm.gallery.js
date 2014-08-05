7167fba2ae9aa7a8f5d47ca90909c94819a31745
vasim <vasim.ahmed@itmonarch.com>
2014-08-05 19:03:53 +0530

    Fix (#124) - disable hover effect for mobiles

--- MvcWebRole1/Content/controls/mm.gallery.js
+++ MvcWebRole1/Content/controls/mm.gallery.js
@@ -16,36 +16,55 @@ var ScaleElement = function (element) {
     }
 
     $(element).find("li.movie").each(function () {
-        $(this).find(".movie-poster,.captionAndNavigate").hover(function () {
-            var element = this;
-            
-            if ($(this).attr("class") == "captionAndNavigate")
-                element = $(this).prev();
+        $(this).find(".movie-poster,.captionAndNavigate").hover(function (e) {
 
-            currentElement = element;
-            $(element).attr("isactive", "true");
-            $(element).attr("org-w", $(element).css("width")).css("width", "300px").css("top", "-35px").css("left", "-50px");
-            $(element).attr("org-h", $(element).css("height")).css("height", "400px");
-            $(element).css("z-index", "100").css("position", "absolute");
-            $(element).parent().css("height", "500px").css("z-index", "98");
-            $(element).parent().find(".captionAndNavigate").css("width", "300px").css("height", "400px").css("z-index", "100").css("top", "-35px").css("left", "-50px");
+            if ($(window).width() < 768) {
+                e.stopPropagation();
+                return false;
+            }
 
-            $(element).parent().find(".movie-synopsis").each(function () {
-                $(this).show();
+                var element = this;
+                
+                if ($(this).attr("class") == "captionAndNavigate")
+                    element = $(this).prev();
+
+                currentElement = element;
+                $(element).attr("isactive", "true");
+                $(element).attr("org-w", $(element).css("width")).css("width", "300px").css("top", "-35px").css("left", "-50px");
+                $(element).attr("org-h", $(element).css("height")).css("height", "400px");
+                $(element).css("z-index", "100").css("position", "absolute");
+                $(element).parent().css("height", "500px").css("z-index", "98");
+                $(element).parent().find(".captionAndNavigate").css("width", "300px").css("height", "400px").css("z-index", "100").css("top", "-35px").css("left", "-50px");
+
+                $(element).parent().find(".movie-synopsis").each(function () {
+                    $(this).show();
+                });
+
+            }, function (e) {
+                if ($(window).width() < 768) {
+                    e.stopPropagation();
+                    return false;
+                }
+
+                var element = this;
+                if ($(this).attr("class") == "captionAndNavigate")
+                    element = $(this).prev();
+
+                 
+                if ($(element).attr("org-h") == "400px") {
+                    $(element).attr("org-h", "340px");
+                    $(element).attr("org-w", "225px");
+                }
+
+
+
+                $(element).css("width", $(element).attr("org-w")).css("height", $(element).attr("org-h")).css("z-index", "1").css("position", "relative").css("top", "0px").css("left", "0px");
+                $(element).parent().css("height", $(element).attr("org-h"));
+                $(element).parent().css("z-index", "1");
+                $(element).parent().find(".captionAndNavigate").css("width", $(element).attr("org-w")).css("top", "auto").css("left", "0px").css("z-index", "1").css("height", "auto");
+                $(element).parent().find(".movie-synopsis").hide();
             });
-
-        }, function () {
-            var element = this;
-
-            if ($(this).attr("class") == "captionAndNavigate")
-                element = $(this).prev();
-
-            $(element).css("width", $(element).attr("org-w")).css("height", $(element).attr("org-h")).css("z-index", "1").css("position", "relative").css("top", "0px").css("left", "0px");
-            $(element).parent().css("height", $(element).attr("org-h"));
-            $(element).parent().css("z-index", "1");
-            $(element).parent().find(".captionAndNavigate").css("width", $(element).attr("org-w")).css("top", "auto").css("left", "0px").css("z-index", "1").css("height", "auto");
-            $(element).parent().find(".movie-synopsis").hide();
-        });
+            //$(this).find(".movie-poster,.captionAndNavigate").unbind("hover");
     });
 }
 
