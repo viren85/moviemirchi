04d54efc299257a46a9e17666528bc36d4dcbbf7
bhangaleashish <bhangaleashish@gmail.com>
2014-08-08 20:12:08 -0700

    This makes lucene deployment autonomous [also fixes ImagePath]

--- MvcWebRole1/WebRole.cs
+++ MvcWebRole1/WebRole.cs
@@ -1,64 +1,22 @@
-﻿using Microsoft.WindowsAzure.ServiceRuntime;
-using System;
-using System.Diagnostics;
-using System.IO;
-
+﻿
 namespace MvcWebRole1
 {
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using System.Diagnostics;
+
     public class WebRole : RoleEntryPoint
     {
+
         public override bool OnStart()
         {
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
-
-            SetupLuceneIndexes();
 
             var result = base.OnStart();
 
             RunSmartMonkey();
 
             return result;
-        }
-
-        private void SetupLuceneIndexes()
-        {
-            try
-            {
-                var separator = new char[] { '_' };
-                var currentDir = System.Configuration.ConfigurationManager.AppSettings["ImagePath"];
-                var luceneDir = Path.Combine(currentDir, "lucene_index");
-                if (Directory.Exists(luceneDir))
-                {
-                    foreach (var file in Directory.EnumerateFiles(luceneDir))
-                    {
-                        try
-                        {
-                            var name = Path.GetFileName(file);
-                            if (name.ToLowerInvariant().Contains("segment"))
-                            {
-                                name = name.TrimStart(separator);
-                            }
-                            else if (name.ToLowerInvariant().Contains(".cfs"))
-                            {
-                                name = name.TrimEnd(separator);
-                            }
-
-                            var newFile = Path.Combine(
-                                Path.GetDirectoryName(file),
-                                name);
-                            File.Move(file, newFile);
-                        }
-                        catch
-                        {
-                        }
-                    }
-                }
-            }
-            catch
-            {
-
-            }
         }
 
         private static void RunSmartMonkey()
--- MvcWebRole2/WebRole.cs
+++ MvcWebRole2/WebRole.cs
@@ -1,20 +1,88 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Diagnostics;
-using Microsoft.WindowsAzure.ServiceRuntime;
 
 namespace MvcWebRole2
 {
+    using Microsoft.Web.Administration;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+    using System.Configuration;
+    using System.IO;
+    using System.Linq;
+
     public class WebRole : RoleEntryPoint
     {
+        // Update this if you are running locally
+        public static string VirtualDirPath = @"C:\Users\bash\Documents\GitHub\moviemirchi\qiouwpeiqwokelwqej";
+
         public override bool OnStart()
         {
             // For information on handling configuration changes
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
+            var path = GetVirtualDirPath();
+            ConfigurationManager.AppSettings["ImagePath"] = WebRole.VirtualDirPath = path;
+
+            SetupLuceneIndexes(path);
+
             return base.OnStart();
+        }
+
+        private string GetVirtualDirPath()
+        {
+            try
+            {
+                using (ServerManager serverManager = new ServerManager())
+                {
+                    var paths =
+                        serverManager.Sites
+                             .SelectMany(site => site.Applications
+                                 .Where(a => a.Path.Contains(RoleEnvironment.CurrentRoleInstance.Role.Name))
+                                 .SelectMany(ar => ar.VirtualDirectories
+                                     .Select(vr => vr.PhysicalPath)))
+                             .FirstOrDefault();
+                    return paths ?? WebRole.VirtualDirPath;
+                }
+            }
+            catch { ;}
+
+            return WebRole.VirtualDirPath;
+        }
+
+        private void SetupLuceneIndexes(string destDir)
+        {
+            try
+            {
+                var separator = new char[] { '_' };
+                var luceneDir = Path.Combine(destDir, "lucene_index");
+                if (Directory.Exists(luceneDir))
+                {
+                    foreach (var file in Directory.EnumerateFiles(luceneDir))
+                    {
+                        try
+                        {
+                            var name = Path.GetFileName(file);
+                            var lname = name.ToLowerInvariant();
+                            if (lname.Contains("segment"))
+                            {
+                                name = name.TrimStart(separator);
+                            }
+                            else if (lname.Contains(".cfs"))
+                            {
+                                name = name.TrimEnd(separator);
+                            }
+
+                            var newFile = Path.Combine(
+                                Path.GetDirectoryName(file),
+                                name);
+                            File.Move(file, newFile);
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
+            catch
+            {
+            }
         }
     }
 }
