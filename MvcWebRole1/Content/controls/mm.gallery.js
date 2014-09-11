fcd4319f15081b5f002a86582a36e6b71fe851bb
vasim <vasim.ahmed@itmonarch.com>
2014-09-11 18:44:17 +0530

    Fix - Alignment of tile elements

--- MvcWebRole1/Content/controls/mm.gallery.js
+++ MvcWebRole1/Content/controls/mm.gallery.js
@@ -23,48 +23,49 @@ var ScaleElement = function (element) {
                 return false;
             }
 
-                var element = this;
-                
-                if ($(this).attr("class") == "captionAndNavigate")
-                    element = $(this).prev();
+            var element = this;
 
-                currentElement = element;
-                $(element).attr("isactive", "true");
-                $(element).attr("org-w", $(element).css("width")).css("width", "300px").css("top", "-35px").css("left", "-50px");
-                $(element).attr("org-h", $(element).css("height")).css("height", "400px");
-                $(element).css("z-index", "100").css("position", "absolute");
-                $(element).parent().css("height", "500px").css("z-index", "98");
-                $(element).parent().find(".captionAndNavigate").css("width", "300px").css("height", "400px").css("z-index", "100").css("top", "-35px").css("left", "-50px");
+            if ($(this).attr("class") == "captionAndNavigate")
+                element = $(this).prev();
 
-                $(element).parent().find(".movie-synopsis").each(function () {
-                    $(this).show();
-                });
+            currentElement = element;
+            $(element).attr("isactive", "true");
+            $(element).attr("org-w", $(element).css("width")).css("width", "300px").css("top", "-35px").css("left", "-50px");
+            $(element).attr("org-h", $(element).css("height")).css("height", "400px");
+            $(element).css("z-index", "100").css("position", "absolute");
+            $(element).parent().css("height", "500px").css("z-index", "98");
+            $(element).parent().find(".captionAndNavigate").css("width", "300px").css("height", "400px").css("z-index", "100").css("top", "-35px").css("left", "-50px");
 
-            }, function (e) {
-                if ($(window).width() < 768) {
-                    e.stopPropagation();
-                    return false;
-                }
+            $(element).parent().find(".additives").css("position", "relative").css("top", "10px").css("float", "right");
 
-                var element = this;
-                if ($(this).attr("class") == "captionAndNavigate")
-                    element = $(this).prev();
-
-                 
-                if ($(element).attr("org-h") == "400px") {
-                    $(element).attr("org-h", "340px");
-                    $(element).attr("org-w", "225px");
-                }
-
-
-
-                $(element).css("width", $(element).attr("org-w")).css("height", $(element).attr("org-h")).css("z-index", "1").css("position", "relative").css("top", "0px").css("left", "0px");
-                $(element).parent().css("height", $(element).attr("org-h"));
-                $(element).parent().css("z-index", "1");
-                $(element).parent().find(".captionAndNavigate").css("width", $(element).attr("org-w")).css("top", "auto").css("left", "0px").css("z-index", "1").css("height", "auto");
-                $(element).parent().find(".movie-synopsis").hide();
+            $(element).parent().find(".movie-synopsis").each(function () {
+                $(this).show();
             });
-            //$(this).find(".movie-poster,.captionAndNavigate").unbind("hover");
+
+        }, function (e) {
+            if ($(window).width() < 768) {
+                e.stopPropagation();
+                return false;
+            }
+
+            var element = this;
+            if ($(this).attr("class") == "captionAndNavigate")
+                element = $(this).prev();
+
+
+            if ($(element).attr("org-h") == "400px") {
+                $(element).attr("org-h", "340px");
+                $(element).attr("org-w", "225px");
+            }
+
+            $(element).css("width", $(element).attr("org-w")).css("height", $(element).attr("org-h")).css("z-index", "1").css("position", "relative").css("top", "0px").css("left", "0px");
+            $(element).parent().css("height", $(element).attr("org-h"));
+            $(element).parent().css("z-index", "1");
+            $(element).parent().find(".captionAndNavigate").css("width", $(element).attr("org-w")).css("top", "auto").css("left", "0px").css("z-index", "1").css("height", "auto");
+            $(element).parent().find(".movie-synopsis").hide();
+            $(element).parent().find(".additives").css("position", "absolute").css("top", "auto").css("bottom", "10px").css("float", "clear").css("right", "10px");
+        });
+        //$(this).find(".movie-poster,.captionAndNavigate").unbind("hover");
     });
 }
 
