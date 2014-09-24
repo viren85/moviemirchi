1c6f7005a5b3f00409360c31300d31103326b8af
Viren <viren@itmonarch.com>
2014-09-24 21:02:08 +0530

    Fix - Running API role

--- APIRole/App_Start/BundleConfig.cs
+++ APIRole/App_Start/BundleConfig.cs
@@ -1,13 +1,13 @@
 ﻿
 namespace CloudMovie.APIRole.Config
 {
-    using System.Web.Optimization;
+    //using System.Web.Optimization;
 
     public class BundleConfig
     {
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
-        public static void RegisterBundles(BundleCollection bundles)
+        /*public static void RegisterBundles(BundleCollection bundles)
         {
-        }
+        }*/
     }
 }
---
+++ APIRole/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace CloudMovie.APIRole
+{
+    using CloudMovie.APIRole.Config;
+    using System;
+    using System.Web.Http;
+    using System.Web.Mvc;
+    //using System.Web.Optimization;
+    using System.Web.Routing;
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
+        }
+    }
+}
