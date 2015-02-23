26e390983aa71cbb6bf210b066fdcaf1f94f6fe2
vasim89 <vasim.ahmed@itmonarch.com>
2015-02-23 19:56:12 +0530

    Made changes in javascript file
    Made changes in mm.admin.artist.js according to new properties, made
    changes in mm.common.css file
    as per discussion with viren

--- MvcWebRole2/Content/controls/mm.admin.artists.js
+++ MvcWebRole2/Content/controls/mm.admin.artists.js
@@ -81,7 +81,24 @@
         var artistName = new FormBuilder().GetTextField("txtArtistName", "Artist Name", "Artist Name");
         var bio = new FormBuilder().GetTextArea("txtBio", "Bio", "Bio");
         var born = new FormBuilder().GetTextArea("txtBorn", "Born", "Born");
-        var popularity = new FormBuilder().GetTextField("txtPopularity", "Popularity", "Popularity");        
+        var popularity = new FormBuilder().GetTextField("txtPopularity", "Popularity", "Popularity");
+        //Below feilds added later on by Ranveer
+        var nickName = new FormBuilder().GetTextField("txtNickName", "Nick Name", "Nick Name");
+        var age = new FormBuilder().GetTextField("txtAge", "Age", "Age");
+        var familyRelation = new FormBuilder().GetTextArea("txtFamilyRelation", "Family Relation", "Family Relation"); //json
+        var dateOfBirth = new FormBuilder().GetTextField("txtDateOfBirth", "Date Of Birth", "Date Of Birth");
+        var bornCity = new FormBuilder().GetTextField("txtBornCity", "Born City", "Born City");
+        var zodiacSign = new FormBuilder().GetTextField("txtZodiacSign", "Zodiac Sign", "Zodiac Sign");
+        var hobbies = new FormBuilder().GetTextArea("txtHobbies", "Hobbies", "Hobbies"); //json
+        var educationalDetails = new FormBuilder().GetTextArea("txtEducationalDetails", "Educational Details", "Educational Details"); //json
+        var socialActivities = new FormBuilder().GetTextArea("txtSocialActivities", "Social Activities", "Social Activities");  //json
+        var debut = new FormBuilder().GetTextField("txtDebut", "Debut", "Debut");
+        var rememberForMovies = new FormBuilder().GetTextArea("txtRememberForMovies", "Remember For Movies", "Remember For Movies"); //json
+        var awards = new FormBuilder().GetTextArea("txtAwards", "Awards", "Awards");  //json
+        var facebook = new FormBuilder().GetTextField("txtFacebook", "Facebook", "Facebook");
+        var twitter = new FormBuilder().GetTextField("txtTwitter", "Twitter", "Twitter");
+        var instagram = new FormBuilder().GetTextField("txtInstagram", "Instagram", "Instagram");
+        var summary = new FormBuilder().GetTextArea("txtSummary", "Summary", "Summary");
 
         $(formContainer).append(isEnabled);
 
@@ -91,10 +108,28 @@
         $(formContainer).append(born);
         $(formContainer).append(popularity);
 
+        // added by ry
+        $(formContainer).append(nickName);
+        $(formContainer).append(age);
+        $(formContainer).append($("<div/>").attr("id", "familylink").attr("style", "width: 100%;").append(this.AddFamilyRelation(true)));
+        $(formContainer).append(dateOfBirth);
+        $(formContainer).append(bornCity);
+        $(formContainer).append(zodiacSign);
+        $(formContainer).append(hobbies);
+        $(formContainer).append($("<div/>").attr("id", "education").attr("style", "width: 100%;").append(this.AddEducationalDetail(true)));
+        $(formContainer).append(socialActivities);
+        $(formContainer).append($("<div/>").attr("id", "debut").attr("style", "width: 100%;").append(this.AddDebut(true)));
+        $(formContainer).append($("<div/>").attr("id", "remembered").attr("style", "width: 100%;").append(this.AddRememberedFor(true)));
+        $(formContainer).append($("<div/>").attr("id", "awards").attr("style", "width: 100%;").append(this.AddAwards(true)));
+        $(formContainer).append(facebook);
+        $(formContainer).append(twitter);
+        $(formContainer).append(instagram);
+        $(formContainer).append(summary);
+
         return formContainer;
     }
 
-    Artists.prototype.PopulateArtistDetails = function (artist) {        
+    Artists.prototype.PopulateArtistDetails = function (artist) {
         console.log(artist);
         //basic info
         $("#txtUnique").val(artist.UniqueName);
@@ -135,7 +170,7 @@
         var uniqueName = $("#txtUnique").val();
         var artistName = $("#txtArtistName").val();
         var bio = $("#txtBio").val();
-        var born = $("#txtBorn").val(); 
+        var born = $("#txtBorn").val();
         var popularity = $("#txtPopularity").val();
 
         if (uniqueName != undefined && uniqueName != "") {
@@ -161,7 +196,7 @@
         //get ratings
         var myScore = { "teekharating": $("#txtTeekhaRate").val(), "feekharating": $("#txtFeekaRate").val(), "criticrating": $("#txtMyScore").val() };
         artist.MyScore = JSON.stringify(myScore);
-        
+
         //geting posters
         var posters = [];
         var selectPoster = null;
@@ -225,12 +260,137 @@
             "RememberForMovies": artist.RememberForMovies,
             "Awards": artist.Awards,
             "FacebookURL": artist.FacebookURL,
-            "InstagramURL": artist.InstagramURL            
+            "InstagramURL": artist.InstagramURL
             //end
         };
 
         //UpdateArtist
         //CallController("../Home/UpdateArtists", "data", artistData, function () { $("#status").html("Artist details saved successfully!"); });
         CallController("api/UpdateArtist", "data", objArtist, function () { $("#status").html("Artist details saved successfully!"); });
+    }
+
+    // added on 23-feb-2015
+    // Family Relation
+    Artists.prototype.AddFamilyRelation = function (isAddButton) {
+        var familyRelation = $("<div/>").attr("style", "width: 100%;margin: px;").attr("class", "Relation");
+        $(familyRelation).append($("<span/>").text("Family Relationship"));
+
+        $(familyRelation).append($("<input/>").attr("type", "text").attr("style", "width:50%").attr("placeholder", "Family Link"));
+
+        var button = $("<div/>");
+
+        if (isAddButton) {
+            $(button).attr("class", "btn btn-success").html("Add ...").click(function () {
+                $("#familylink").append(new Artists().AddFamilyRelation(false));
+            });
+
+        } else {
+            $(button).attr("class", "btn btn-danger").html("X").click(function () {
+                $(familyRelation).remove();
+            });
+        }
+
+        $(familyRelation).append(button);
+
+        return familyRelation;
+    }
+
+    // Educational Detail
+    Artists.prototype.AddEducationalDetail = function (isAddButton) {
+        var educationalDetail = $("<div/>").attr("style", "width: 100%;margin: px;").attr("class", "Educational");
+        $(educationalDetail).append($("<span/>").text("Educational Detail"));
+        $(educationalDetail).append($("<input/>").attr("type", "text").attr("style", "width:50%").attr("placeholder", "Education"));
+
+        var button = $("<div/>");
+
+        if (isAddButton) {
+            $(button).attr("class", "btn btn-success").html("Add ...").click(function () {
+                $("#education").append(new Artists().AddEducationalDetail(false));
+            });
+
+        } else {
+            $(button).attr("class", "btn btn-danger").html("X").click(function () {
+                $(educationalDetail).remove();
+            });
+        }
+
+        $(educationalDetail).append(button);
+
+        return educationalDetail;
+    }
+
+    // Debut Film
+    Artists.prototype.AddDebut = function (isAddButton) {
+        var debut = $("<div/>").attr("style", "width: 100%;margin: px;").attr("class", "Debut_Start");
+        $(debut).append($("<span/>").text("Debut"));
+        $(debut).append($("<input/>").attr("type", "text").attr("style", "width:50%").attr("placeholder", "Debut"));
+
+
+
+
+        var button = $("<div/>");
+
+        if (isAddButton) {
+            $(button).attr("class", "btn btn-success").html("Add ...").click(function () {
+                $("#debut").append(new Artists().AddDebut(false));
+            });
+
+        } else {
+            $(button).attr("class", "btn btn-danger").html("X").click(function () {
+                $(debut).remove();
+            });
+        }
+
+        $(debut).append(button);
+
+        return debut;
+    }
+
+    // Remembered For
+    Artists.prototype.AddRememberedFor = function (isAddButton) {
+        var rememberedFor = $("<div/>").attr("style", "width: 100%;margin: px;").attr("class", "Remember");
+        $(rememberedFor).append($("<span/>").text("Remembered For"));
+        $(rememberedFor).append($("<input/>").attr("type", "text").attr("style", "width:50%").attr("placeholder", "Remembered"));
+
+        var button = $("<div/>");
+
+        if (isAddButton) {
+            $(button).attr("class", "btn btn-success").html("Add ...").click(function () {
+                $("#remembered").append(new Artists().AddRememberedFor(false));
+            });
+
+        } else {
+            $(button).attr("class", "btn btn-danger").html("X").click(function () {
+                $(rememberedFor).remove();
+            });
+        }
+
+        $(rememberedFor).append(button);
+
+        return rememberedFor;
+    }
+
+    // Awards
+    Artists.prototype.AddAwards = function (isAddButton) {
+        var awards = $("<div/>").attr("style", "width: 100%;margin: px;").attr("class", "Awards Received");
+        $(awards).append($("<span/>").text("Awards"));
+        $(awards).append($("<input/>").attr("type", "text").attr("style", "width:50%").attr("placeholder", "Awards"));
+
+        var button = $("<div/>");
+
+        if (isAddButton) {
+            $(button).attr("class", "btn btn-success").html("Add ...").click(function () {
+                $("#awards").append(new Artists().AddAwards(false));
+            });
+
+        } else {
+            $(button).attr("class", "btn btn-danger").html("X").click(function () {
+                $(awards).remove();
+            });
+        }
+
+        $(awards).append(button);
+
+        return awards;
     }
 }
