c25732fd7c0f8d40193190d86211acf059226f6d
vasim89 <vasim.ahmed@itmonarch.com>
2015-04-01 18:07:33 +0530

    Added new page for Bing Image
    Added new page for crawl images form bing site.
    Added API for getting data.
    Added Crawl class.
    Added Jss and css for showing bing image of our site.

---
+++ APIRole/Controllers/api/CrawlBingImagesController.cs
@@ -0,0 +1,63 @@
+﻿
+namespace CloudMovie.APIRole.Controllers.api
+{
+    using CloudMovie.APIRole.API;
+    using CloudMovie.APIRole.UDT;
+    using DataStoreLib.Constants;
+    using DataStoreLib.Models;
+    using DataStoreLib.Storage;
+    using DataStoreLib.Utils;
+    using LuceneSearchLibrary;
+    using MovieCrawler;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Script.Serialization;
+
+    public class CrawlBingImagesController : BaseController
+    {
+        private static Lazy<JavaScriptSerializer> jsonSerializer = new Lazy<JavaScriptSerializer>(() => new JavaScriptSerializer());
+        protected override string ProcessRequest()
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+
+            try
+            {
+               
+                // get query string parameters
+                var qpParams = HttpUtility.ParseQueryString(this.Request.RequestUri.Query);
+
+                if (string.IsNullOrEmpty(qpParams["n"]))
+                {
+                    throw new ArgumentException(Constants.API_EXC_MOVIE_NAME_NOT_EXIST);
+                }
+
+                // get the bing images url
+                string searchTerm = qpParams["n"].ToString();
+                string bingURL = "http://www.bing.com/images/search?&q=" + searchTerm;
+
+               // string bingURL = "http://www.bing.com/images/search?sid=E8BCC6199DA2412BA5030B63AD02B439&q=" + searchTerm + "&qft=+filterui:license-L2_L3_L4&FORM=R5IR39";
+
+                List<string> bingPosters = new BingCrawler().GetMoviePosterUrls(bingURL);
+
+                if (bingPosters != null)
+                {
+                    
+                    return  json.Serialize(bingPosters);
+                }
+            }
+            catch (Exception ex)
+            {
+                // if any error occured then return User friendly message with system error message
+                return json.Serialize(new { Status = "Error", UserMessage = "Error occured while getting posters", ActualError = ex.Message });
+            }
+
+            // if movie is null then return single object.
+            return string.Empty;
+        }
+
+    }
+}
---
+++ MvcWebRole2/Content/controls/mm.admin.bingimage.js
@@ -0,0 +1,40 @@
+﻿var BingPosters = function () {
+
+    BingPosters.prototype.GetPosterContainer = function (posters) {
+        var container = $("<div/>").attr("class", "form-container");
+        var sectionTitle = new MovieInformation().GetMovieInfoContainer("poster-section-title", "Bing Images");
+
+        return $(container).append(sectionTitle).append(this.GetAllPoster(posters));
+    }
+
+
+    BingPosters.prototype.GetAllPoster = function (posters) {
+
+        var container = $("<div/>").attr("class", "poster-container");
+
+        if (posters != null && posters != undefined && posters.length > 0) {
+            var isChecked = false;
+            for (i = 0; i < posters.length; i++) {
+                var innerContainer = $("<div/>").attr("class", "bing-img-container");
+                var square = new FormBuilder().GetCheckBox(posters[i], "", isChecked);
+                var img = $("<img/>").attr("src", posters[i]).attr("class", "single-poster");
+
+                $(innerContainer).append(square).append(img);
+
+                //$(container).append(innerContainer).append(square).append(img);
+                $(container).append(innerContainer);
+                Counter = i;
+            }
+        }
+        else {
+            $(container).append($("<div/>").html("No result found for '" + $("#txtSearchTerm").val() + "'")).attr("style", "  width: 90%;margin-left: 3%;");;
+        }
+
+        return container;
+    }
+}
+
+
+
+
+
