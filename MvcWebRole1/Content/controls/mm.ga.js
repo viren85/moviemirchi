49d5e0e744fd1df1c9b9e7af2f1521eca1bf154f
bhangaleashish <bhangaleashish@gmail.com>
2015-04-25 12:19:01 -0700

    Bring back GA to layout from its own js script

--- MvcWebRole1/Content/controls/mm.ga.js
+++ MvcWebRole1/Content/controls/mm.ga.js
@@ -1,15 +1,4 @@
-﻿(function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
-        (i[r].q = i[r].q || []).push(arguments)
-    }, i[r].l = 1 * new Date(); a = s.createElement(o),
-    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-ga('create', 'UA-52983576-1', 'auto');
-ga('require', 'displayfeatures');
-ga('send', 'pageview');
-
-var USER_ID = "";
+﻿// Google analytics is set up in the layout
 
 var trackNewsLink = function (url) {
     ga('send', 'event', 'news', 'click', url);
